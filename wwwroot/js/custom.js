1c147ae9b3ace9707269460e0be6608b23ce4f7e
Lovas Bálint <lovasb93@gmail.com>
2019-03-10 14:53:19 +0100

    Az indexoldalon a fájl mentés és betöltés stílusa javítva. Az SQL kifejezésekre kattintva beíródik a szövegdobozba. Az eredmények oldalról a custom.js eltávolítva.

--- wwwroot/js/custom.js
+++ wwwroot/js/custom.js
@@ -3,31 +3,89 @@ $(function() {
     addEventsForDatabaseList();
     addEventsForTableList();
     addEventsForColumnList();
-    sqlKeywords();
-    sqlTextarea();
-
-    document.getElementById("sqlFile").addEventListener("change", function () {
-        let formData = new FormData();
-        formData.append('sqlFile', $('#sqlFile')[0].files[0]);
-        let _url = '@Url.Action("Upload", "HomeController")';
-        $.ajax({
-            type: 'POST',
-            url: '/Home/Upload',
-            contentType: false,
-            processData: false,
-            data: formData,
-            success: function (result) {
-                console.log(result);
-                let textarea = document.getElementById("sqlTextarea");
-                textarea.value = result;
-            }
-        });
-    });
-
+    addEventForFileUpload();
+    addEventsForSqlExpressions();
+    addEventsForSqlKeywords();
+    addEventsForSqlTextarea();
+    
 });
 
 
 let dragObject;
+let selectedColumns = [];
+let selectedTable = "";
+
+function addEventsForSqlExpressions() {
+    document.getElementById("select").addEventListener("click", function () {
+        let query = "SELECT ";
+        for (let i = 0; i < selectedColumns.length; i++) {
+            if (i < (selectedColumns.length - 1))
+                query += "[" + selectedColumns[i] + "], ";
+            else
+                query += "[" + selectedColumns[i] + "] ";
+        }
+        query += "FROM " + "[" + selectedTable + "]";
+        console.log(query);
+        document.getElementById("sqlTextarea").value = query;
+    });
+
+    document.getElementById("selectTop").addEventListener("click", function () {
+        let query = "SELECT TOP(1000) ";
+        for (let i = 0; i < selectedColumns.length; i++) {
+            if (i < (selectedColumns.length - 1))
+                query += "[" + selectedColumns[i] + "], ";
+            else
+                query += "[" + selectedColumns[i] + "] ";
+        }
+        query += "FROM " + "[" + selectedTable + "]";
+        console.log(query);
+        document.getElementById("sqlTextarea").value = query;
+    });
+
+    document.getElementById("insert").addEventListener("click", function () {
+        let query = "INSERT INTO " + "[" + selectedTable + "] (";
+        for (let i = 0; i < selectedColumns.length; i++) {
+            if (i < (selectedColumns.length - 1))
+                query += "[" + selectedColumns[i] + "], ";
+            else
+                query += "[" + selectedColumns[i] + "] ";
+        }
+        query += ") VALUES (";
+        for (let i = 0; i < selectedColumns.length; i++) {
+            if (i < (selectedColumns.length - 1))
+                query += "'', ";
+            else
+                query += "''";
+        }
+        query += ")";
+        console.log(query);
+        document.getElementById("sqlTextarea").value = query;
+    });
+
+    document.getElementById("update").addEventListener("click", function () {
+        let query = "UPDATE " + "[" + selectedTable + "] SET ";
+        for (let i = 0; i < selectedColumns.length; i++) {
+            if (i < (selectedColumns.length - 1))
+                query += "[" + selectedColumns[i] + "] = '', ";
+            else
+                query += "[" + selectedColumns[i] + "] = ''";
+        }
+        console.log(query);
+        document.getElementById("sqlTextarea").value = query;
+    });
+
+    document.getElementById("delete").addEventListener("click", function () {
+        let query = "DELETE FROM " + "[" + selectedTable + "] WHERE ";
+        for (let i = 0; i < selectedColumns.length; i++) {
+            if (i < (selectedColumns.length - 1))
+                query += "[" + selectedColumns[i] + "] = '' AND ";
+            else
+                query += "[" + selectedColumns[i] + "] = ''";
+        }
+        console.log(query);
+        document.getElementById("sqlTextarea").value = query;
+    });
+}
 
 function addEventsForDatabaseList() {
     document.getElementById("databases").addEventListener("dblclick", function () {
@@ -80,6 +138,7 @@ function addEventsForTableList() {
     document.getElementById("tables").addEventListener("change", function () {
         let databaseName = document.getElementById("databases").value;
         let tableName = document.getElementById("tables").value;
+        selectedTable = tableName;
         let columns = document.getElementById("columns");
 
         while (columns.hasChildNodes()) {
@@ -113,9 +172,37 @@ function addEventsForColumnList() {
         let sqlTextBox = document.getElementById("sqlTextarea");
         sqlTextBox.value += "[" + x + "] ";
     });
+
+    document.getElementById("columns").addEventListener("change", function () {
+        let values = $("#columns").val();
+        selectedColumns = values;
+        console.log(selectedColumns);
+        //for (let i = 0; i < values.length; i++)
+        //    console.log(values[i]);
+    });
 }
 
-function sqlKeywords() {
+function addEventForFileUpload() {
+    document.getElementById("sqlFile").addEventListener("change", function () {
+        let formData = new FormData();
+        formData.append('sqlFile', $('#sqlFile')[0].files[0]);
+        let _url = '@Url.Action("Upload", "HomeController")';
+        $.ajax({
+            type: 'POST',
+            url: '/Home/Upload',
+            contentType: false,
+            processData: false,
+            data: formData,
+            success: function (result) {
+                console.log(result);
+                let textarea = document.getElementById("sqlTextarea");
+                textarea.value = result;
+            }
+        });
+    });
+}
+
+function addEventsForSqlKeywords() {
   let object = document.getElementById('sqlKeywords');
   let length = object.childNodes.length;
 
@@ -135,7 +222,7 @@ function sqlKeywords() {
   }
 }
 
-function sqlTextarea() {
+function addEventsForSqlTextarea() {
   let object = document.getElementById('sqlTextarea');
 
   object.addEventListener("drop", function(ev) {
